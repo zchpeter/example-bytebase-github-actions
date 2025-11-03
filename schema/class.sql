a98982d24913f93c3ccadb58f52b1b315c4d3879
zchpeter <zchpeter@gmail.com>
2025-11-03 06:22:17 -0500

    fix: use default

--- schema/class.sql
+++ schema/class.sql
@@ -2,7 +2,7 @@ CREATE TABLE "public"."class" (
     "class_no" integer DEFAULT nextval('public.class_no_seq'::regclass) NOT NULL,
     "class_name" text NOT NULL,
     "grade" integer NOT NULL,
-    "student_limit" integer NOT NULL,
+    "min_credit" integer NOT NULL DEFAULT 0,
     CONSTRAINT "class_pkey" PRIMARY KEY (class_no),
     CONSTRAINT "class_grade_check" CHECK (grade > 0 AND grade <= 12)
 );
